3e25d26079ec69d3630fc618ceb271425fa9d0cf
LMQ <LMQ@zhiziyun-PC>
2017-05-24 11:56:27 +0800

    添加KuduContext

--- src/main/scala/com/impala/kudu/test/KuduClientTest.scala
+++ src/main/scala/com/impala/kudu/test/KuduClientTest.scala
@@ -4,23 +4,36 @@ import org.apache.kudu.client.KuduClient
 import org.apache.kudu.client.KuduClient.KuduClientBuilder
 import scala.collection.JavaConversions._
 import org.apache.kudu.client.KuduPredicate
+import org.apache.kudu.client.KuduScanToken
 object KuduClientTest {
   def main(args: Array[String]): Unit = {
-    val client=new KuduClientBuilder("kylin-master2").build()
-    val table=client.openTable("impala::default.kudu_pc_log")
+
+  }
+  def writeKudu() {
+
+    
+    
+    
+  }
+  def scanKudu() {
+    val client = new KuduClientBuilder("kylin-master2").build()
+    val table = client.openTable("impala::default.kudu_pc_log")
     client.getTablesList.getTablesList.foreach { println }
-    val scanner = client.newScanTokenBuilder(table).build()
-    scanner.foreach { sk =>
-      
-      println(sk.getTablet.getPartition)
-      println( sk.getTablet.getReplicas)
-      
-    
+    val schema = table.getSchema();
+    val kp = KuduPredicate.newComparisonPredicate(schema.getColumn("siteid"), KuduPredicate.ComparisonOp.EQUAL, "nVdTH0P1TkB")
+    val scanner = client.newScanTokenBuilder(table)
+      .addPredicate(kp)
+      .limit(100)
+      .build()
+    val token = scanner.get(0)
+    val scan = KuduScanToken.deserializeIntoScanner(token.serialize(), client)
+    while (scan.hasMoreRows()) {
+      val results = scan.nextRows()
+      while (results.hasNext()) {
+        val rowresult = results.next();
+        println(rowresult.getString("siteid"))
+      }
     }
-    
-                    
-    
-    
-    
+
   }
 }
