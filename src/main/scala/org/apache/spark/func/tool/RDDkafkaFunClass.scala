59b2d121784b02150eb9290d4efb5b9ac6c8ac99
LMQ <LMQ>
2018-03-07 14:53:24 +0800

    增加注释

--- src/main/scala/org/apache/spark/func/tool/RDDkafkaFunClass.scala
+++ src/main/scala/org/apache/spark/func/tool/RDDkafkaFunClass.scala
@@ -2,7 +2,13 @@ package org.apache.spark.func.tool
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.kafka.KafkaSparkStreamManager
-
+/**
+ * @author LMQ
+ * @description 为rdd增加以下隐式转换。
+ * @attention 如果要使用这部分的功能，需要你继承这个类。
+ * 					  但由于我现在大部分使用的是自定义的  KafkaRDD 。这部分其实用不到。
+ * 					Dstream里面的RDD 更新offset的操作已经放在StreamingKafkaContext类里面了
+ */
 class rddkafkaFunClass[T](rdd: RDD[T]) {
   def updateOffsets(kp: Map[String, String], groupid: String){
     KafkaSparkStreamManager.updateRDDOffset(kp, groupid, rdd)
