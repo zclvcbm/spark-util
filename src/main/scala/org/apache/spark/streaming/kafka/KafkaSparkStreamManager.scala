e2e101fbcd5bbaa8278e8b49893ecf8086b2abc4
LMQ <LMQ>
2018-03-07 14:16:56 +0800

    增加注释，整理结构

--- src/main/scala/org/apache/spark/streaming/kafka/KafkaSparkStreamManager.scala
+++ src/main/scala/org/apache/spark/streaming/kafka/KafkaSparkStreamManager.scala
@@ -21,7 +21,7 @@ import org.apache.spark.common.util.KafkaConfiguration
  * @description 用于spark streaming 读取kafka数据
  */
 private[spark] object KafkaSparkStreamManager
-    extends KafkaSparkTool {
+    extends SparkKafkaManagerBase{
   logname = "StreamingKafkaManager"
   /**
    * @author LMQ
@@ -112,32 +112,6 @@ private[spark] object KafkaSparkStreamManager
     kp: Map[String, String],
     topics: Map[String, Int]) = {
     KafkaUtils.createStream[K, V, U, T](ssc, kp, topics, StorageLevel.MEMORY_ONLY)
-  }
-  /**
-   * @author LMQ
-   * @description  默认的一个handle (key,value)=>(topic,msg)
-   */
-  def msgHandle = (mmd: MessageAndMetadata[String, String]) => (mmd.topic, mmd.message)
-  /**
-   * @author LMQ
-   * @description 获取RDD的offset。但这个rdd必须继承HasOffsetRanges 的
-   */
-  def getRDDConsumerOffsets[T](rdd: RDD[T]) = {
-    var consumoffsets = Map[TopicAndPartition, Long]()
-    val offsetsList = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
-    for (offsets <- offsetsList) {
-      val topicAndPartition = TopicAndPartition(offsets.topic, offsets.partition)
-      consumoffsets += ((topicAndPartition, offsets.untilOffset))
-    }
-    consumoffsets
-  }
-  /**
-   * @author LMQ
-   * @description 将rdd的offset更新至zookeeper
-   */
-  def updateRDDOffset[T](kp: Map[String, String], groupId: String, rdd: RDD[T]) {
-    val offsets = getRDDConsumerOffsets(rdd)
-    updateConsumerOffsets(kp, groupId, offsets)
   }
 
 }
