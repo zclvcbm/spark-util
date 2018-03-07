46f1e5c547d12d6e0f8a2591419e4c20e28e9618
LMQ <LMQ>
2018-03-07 14:54:33 +0800

    增加方法注释

--- src/main/scala/org/apache/spark/kafka/writer/RDDKafkaWriter.scala
+++ src/main/scala/org/apache/spark/kafka/writer/RDDKafkaWriter.scala
@@ -5,6 +5,10 @@ import java.util.Properties
 import org.apache.kafka.clients.producer.ProducerRecord
 private[spark]
 class rddKafkaWriter[T](@transient private val rdd: RDD[T]) {
+ /**
+  * @author LMQ
+  * @description 将rdd的数据写入kafka 
+  */
   def writeToKafka[K, V](
     producerConfig: Properties,
     transformFunc: T => ProducerRecord[K, V]) {
